2aa2621bee343fe0410d4e60ac39bc0abafe97db
vinisoos <107116615+vsoos@users.noreply.github.com>
2022-09-08 17:55:13 +0200

    Word list added from randomlists.com/data/words.json

---
+++ word_list.py
@@ -0,0 +1,19 @@
+# list of the words the user can guess
+
+words = ["teeth", "telephone", "telling", "temper", "temporary", "tempt", "ten", "tendency", "tender", "tense", "tent",
+         "tenuous", "terrible", "terrific", "terrify", "territory", "test", "tested", "testy", "texture", "thank",
+         "thankful", "thaw", "theory", "therapeutic", "thick", "thin", "thing", "things", "thinkable", "third",
+         "thirsty", "thought", "thoughtful", "thoughtless", "thread", "threatening", "three", "thrill", "throat",
+         "throne", "thumb", "thunder", "thundering", "tick", "ticket", "tickle", "tidy", "tie", "tiger", "tight",
+         "tightfisted", "time", "tin", "tiny", "tip", "tire", "tired", "tiresome", "title", "toad", "toe", "toes",
+         "tomatoes", "tongue", "tooth", "toothbrush", "toothpaste", "toothsome", "top", "torpid", "touch", "tough",
+         "tour", "tow", "towering", "town", "toy", "toys", "trace", "trade", "trail", "train", "trains", "tramp",
+         "tranquil", "transport", "trap", "trashy", "travel", "tray", "treat", "treatment", "tree", "trees", "tremble",
+         "tremendous", "trick", "tricky", "trip", "trite", "trot", "trouble", "troubled", "trousers", "truck", "trucks",
+         "truculent", "true", "trust", "truthful", "try", "tub", "tug", "tumble", "turkey", "turn", "twig", "twist",
+         "two", "type", "typical", "ubiquitous", "ugliest", "ugly", "ultra", "umbrella", "unable", "unaccountable",
+         "unadvised", "unarmed", "unbecoming", "unbiased", "uncle", "uncovered", "understood", "underwear",
+         "undesirable", "undress", "unequal", "unequaled", "uneven", "unfasten", "unhealthy", "uninterested", "unique",
+         "unit", "unite", "unkempt", "unknown", "unlock", "unnatural", "unpack", "unruly", "unsightly", "unsuitable",
+         "untidy", "unused", "unusual", "unwieldy", "unwritten", "upbeat", "uppity", "upset", "uptight", "use", "used",
+         "useful", "useless", "utopian", "utter", "uttermost", "vacation", "vacuous", "vagabond", "vague"]
